a3ed2ac182042cea63b7f7ed62022e7f2b5cb2c2
alfonso <alfonso500950@gmail.com>
2018-11-19 17:50:21 +0100

    HECHO

--- app/src/main/java/com/example/usuario/ejemplointentcamera/MainActivity.java
+++ app/src/main/java/com/example/usuario/ejemplointentcamera/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Environment;
 import android.os.StrictMode;
@@ -17,7 +18,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 import java.io.File;
 import java.io.IOException;
@@ -34,34 +37,22 @@ public class MainActivity extends AppCompatActivity {
     static final int VENGO_DE_LA_CAMARA = 1;
     static final int VENGO_DE_LA_CAMARA_CON_FICHERO = 2;
     static final int PEDI_PERMISOS_DE_ESCRITURA = 2;
-Button captura, captura2;
-ImageView imageViewFoto;
+Button captura2;
+VideoView video;
 String rutaFotoActual;
+MediaController control;
+Intent haceFoto;
+String ruta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
         StrictMode.setVmPolicy(builder.build());
-        captura = findViewById(R.id.buttonCaptura);
         captura2 = findViewById(R.id.buttonCaptura2);
-        imageViewFoto = findViewById(R.id.imageViewFoto);
+        video = findViewById(R.id.musica);
+        control = new MediaController(this);
 
-        captura.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent haceFoto = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-                if (haceFoto.resolveActivity(getPackageManager())!=null){
-                    startActivityForResult(haceFoto,VENGO_DE_LA_CAMARA);
-                }else{
-                    Toast.makeText(MainActivity.this, "Necesito un programa de hacer fotos.", Toast.LENGTH_SHORT).show();
-                }
-
-
-            }
-        });
         captura2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -77,21 +68,31 @@ String rutaFotoActual;
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
 
         if ((requestCode == VENGO_DE_LA_CAMARA) && (resultCode == RESULT_OK)){
-            Bundle extras = data.getExtras();
-            Bitmap foto = (Bitmap) extras.get("data");
-            imageViewFoto.setImageBitmap(foto);
+            // no ocurre nada si cancelo el video
+
         }else if ((requestCode == VENGO_DE_LA_CAMARA_CON_FICHERO) && (resultCode == RESULT_OK)){
             //He hecho la foto y la he guardado, así que la foto estará en  rutaFotoActual
-            imageViewFoto.setImageBitmap(BitmapFactory.decodeFile(rutaFotoActual));
-
+            video.setMediaController(control);
+            Uri myuri = Uri.parse(ruta);
+            video.requestFocus();
+            video.setVideoURI(myuri);
+            video.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    control.show();
+                }
+            });
         }
 
         }
     public void capturarFoto( ) {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 5);
+        intent.putExtra(MediaStore.EXTRA_FINISH_ON_COMPLETION, false);
         File ficheroFoto = null;
         try {
             ficheroFoto = crearFicheroImagen();
+            ruta = ficheroFoto.getAbsolutePath();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -105,8 +106,8 @@ String rutaFotoActual;
     File crearFicheroImagen() throws IOException {
         String fechaYHora = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String nombreFichero = "Ejemplo_"+fechaYHora;
-        File carpetaParaFotos = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-        File imagen = File.createTempFile(nombreFichero, ".jpg", carpetaParaFotos);
+        File carpetaParaFotos = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES);
+        File imagen = File.createTempFile(nombreFichero, ".mp4", carpetaParaFotos);
         rutaFotoActual = imagen.getAbsolutePath();
         return imagen;
     }
